a33fdf7cd826002f575305f60344d450a110fbe6
huijon <huijon02@gmail.com>
2014-02-23 21:59:16 +1100

    Don't let it reboot faster than every 5 minutes.

--- dcs6010L-fix.js
+++ dcs6010L-fix.js
@@ -12,7 +12,10 @@ var host = "http://cammy-api.herokuapp.com/";
 var camera_ip = "http://192.168.1.2";
 
 var restarts = 0;
-var last_reboot = new Date().getTime();
+var last_reboot = new Date().getTime() - 5 * 60 * 1000;
+
+var dont_reboot_more_often_than_every_x_minutes = 4,
+        hasnt_sent_anything_in_the_last_y_minutes = 2.5;
 
 cycle = function cycle(callback) {
     //check that the camera admin interface is actually up first...
@@ -27,29 +30,30 @@ cycle = function cycle(callback) {
     }, function(error, response, body) {
         //console.log("Got back body: ", body);
 
-        //give it at least 5 minutes since last reboot..
-        if ((new Date().getTime() - last_reboot) / (1000 * 60) < 5) {
-            if (body) {
-                console.log("Admin interface is responding");
-                request.get({
-                    url: host + "cameras/1NJCM5/events?max_count=1&sort=-1&access_token=6a0cc941e4e7297d0a3fc199b0fc2f7f484624e6578b805b",
-                    json: true
-                },
-                function(error, response, body) {
-                    var snapshots, last_snapshots, last_date, now, diff, res;
-                    res = body.response[0];
-                    snapshots = res.snapshots;
-                    last_snapshots = snapshots[res.end_timestamp * 1000 + ""];
-                    last_date = new Date(res.end_timestamp * 1000);
-                    now = new Date().getTime();
-                    diff = (now - last_date) / (1000 * 60); //in minutes
-                    console.log("It's been " + diff + " minutes since we last got a snapshot from the camera..");
-
-                    //if the camera hasn't sent anything in the last 3 minutes, restart it!
-                    if (diff > 3) {
+        if (body) {
+            process.stdout.write("*");
+            request.get({
+                url: host + "cameras/1NJCM5/events?max_count=1&sort=-1&access_token=6a0cc941e4e7297d0a3fc199b0fc2f7f484624e6578b805b",
+                json: true
+            },
+            function(error, response, body) {
+                var snapshots, last_snapshots, last_date, now, diff, res, time_since_last_reboot;
+                res = body.response[0];
+                snapshots = res.snapshots;
+                last_snapshots = snapshots[res.end_timestamp * 1000 + ""];
+                last_date = new Date(res.end_timestamp * 1000);
+                now = new Date().getTime();
+                diff = (now - last_date) / (1000 * 60); //in minutes
+                //console.log("It's been " + diff + " minutes since we last got a snapshot from the camera..");
+                time_since_last_reboot = (new Date().getTime() - last_reboot) / (1000 * 60);
+                console.log("\nx = " + time_since_last_reboot + "\ny = " + diff);
+                //give it at least x minutes since last reboot..
+                if (time_since_last_reboot > dont_reboot_more_often_than_every_x_minutes) {
+                    //if the camera hasn't sent anything in the last y minutes, restart it!
+                    if (diff > hasnt_sent_anything_in_the_last_y_minutes) {
                         last_reboot = new Date().getTime();
                         restarts++;
-                        console.log("Restarting dcs-6010L for the " + restarts + "th time now!!");
+                        console.log("\nRestarting dcs-6010L for the " + restarts + "th time now!!");
                         request.get({
                             url: camera_ip + "/vb.htm?language=ie&setallreboot=1",
                             auth: {
@@ -64,18 +68,20 @@ cycle = function cycle(callback) {
                             return callback(null);
                         });
                     }
-                });
-            } else {
-                //console.log("Admin interface not back online yet...");
-                process.stdout.write(".");
-                return callback(null);
-            }
-        } else {
-            process.stdout.write(".");
-            return process.nextTick(function() {
-                return callback(null);
+                } else {
+                    //console.log("time_since_last_reboot: " + time_since_last_reboot + " minutes.");
+                    process.stdout.write(".");
+                    return process.nextTick(function() {
+                        return callback(null);
+                    });
+                }
             });
+        } else {
+            //console.log("Admin interface not back online yet...");
+            process.stdout.write(".");
+            return callback(null);
         }
+
     });
 };
 
